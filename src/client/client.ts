18a31009c41ba5a5259c0fc4ab608142c9db1326
Jakub Mucha <jakub@nash.io>
2020-04-26 17:28:45 +0200

    clearer doc for legacyLogin
    Signed-off-by: Jakub Mucha <jakub@nash.io>

--- src/client/client.ts
+++ src/client/client.ts
@@ -941,7 +941,7 @@ export class Client {
    * Legacy login against the central account service. Note: you should prefer to use an API key with the `login` method.
    *
    * Be careful about using this feature, private keys are derived using the password.
-   * So this technically gives full access to the account. Because of this the following features are not supported using the login.
+   * So this technically gives full access to the account. Because of this the following features are not supported using legacy login.
    *
    *  - transferToExternal
    *  - depositToTradingContract
